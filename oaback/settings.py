ec007c8667f9f9a3529acc7f81932cfc9d1c1aa4
tsaydust <dctoash@foxmail.com>
2025-03-07 14:01:18 +0800

    设置debug为false

--- oaback/settings.py
+++ oaback/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = 'django-insecure-d(kbkj(=^la-@v9v9(0q0gzl=8s(xcw6=k)ri2n+tr3ae!!%y8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
